694dad431e3ed8c67c9bef88d6c53afdb5213e87
xaviergonz <xaviergonz@gmail.com>
2025-10-17 11:41:38 +0200

    organize exports in index.ts for improved readability

--- packages/mobx-bonsai/src/index.ts
+++ packages/mobx-bonsai/src/index.ts
@@ -1,49 +1,35 @@
+// biome-ignore-all assist/source/organizeImports: explicit export organization
+
 // mobx-bonsai core
 export { MobxBonsaiError } from "./error/MobxBonsaiError"
-// node
-export { clone } from "./node/clone"
-export { computedProp } from "./node/computedProp"
-export { type Context, createContext } from "./node/context"
-export {
-  assertIsNode,
-  isFrozenNode,
-  isNode,
-  type NodeChange,
-  type NodeChangeListener,
-  node,
-  onDeepChange,
-} from "./node/node"
+
 // node/nodeTypeKey
 export type { BaseNodeType } from "./node/nodeTypeKey/BaseNodeType"
 export type { NodeForNodeType } from "./node/nodeTypeKey/NodeForNodeType"
 export {
-  type AnyNodeType,
-  type AnyTypedNodeType,
-  type AnyUntypedNodeType,
   findNodeTypeById,
   getNodeTypeAndKey,
   getNodeTypeId,
-  type NodeKeyValue,
-  type NodeTypeKey,
-  type NodeTypeValue,
-  type NodeWithAnyType,
   nodeType,
   nodeTypeKey,
   onInit,
+  type AnyNodeType,
+  type AnyTypedNodeType,
+  type AnyUntypedNodeType,
+  type NodeTypeKey,
+  type NodeKeyValue,
+  type NodeTypeValue,
+  type NodeWithAnyType,
   type TNode,
 } from "./node/nodeTypeKey/nodeType"
 export type { TypedNodeType } from "./node/nodeTypeKey/TypedNodeType"
 export type { UntypedNodeType } from "./node/nodeTypeKey/UntypedNodeType"
-// node/snapshot
-export { applySnapshot } from "./node/snapshot/applySnapshot"
-export { getSnapshot } from "./node/snapshot/getSnapshot"
-export { type OnSnapshotListener, onSnapshot } from "./node/snapshot/onSnapshot"
-export { substituteNodeKeys } from "./node/substituteNodeKeys"
-export type { FoundParentPath } from "./node/tree/FoundParentPath"
+
 // node/tree
 export { findChildren } from "./node/tree/findChildren"
 export { findParent } from "./node/tree/findParent"
 export { findParentPath } from "./node/tree/findParentPath"
+export type { FoundParentPath } from "./node/tree/FoundParentPath"
 export { getChildrenNodes } from "./node/tree/getChildrenNodes"
 export { getParent } from "./node/tree/getParent"
 export { getParentPath } from "./node/tree/getParentPath"
@@ -56,33 +42,57 @@ export { isRoot } from "./node/tree/isRoot"
 export { onChildAttachedTo } from "./node/tree/onChildAttachedTo"
 export type { ParentPath } from "./node/tree/ParentPath"
 export type { Path, PathElement, WritablePath } from "./node/tree/pathTypes"
-export type { RootPath } from "./node/tree/RootPath"
 export { resolvePath } from "./node/tree/resolvePath"
-export { WalkTreeMode, walkTree } from "./node/tree/walkTree"
+export type { RootPath } from "./node/tree/RootPath"
+export { walkTree, WalkTreeMode } from "./node/tree/walkTree"
+
+// node/snapshot
+export { applySnapshot } from "./node/snapshot/applySnapshot"
+export { getSnapshot } from "./node/snapshot/getSnapshot"
+export { onSnapshot, type OnSnapshotListener } from "./node/snapshot/onSnapshot"
+
+// node
+export { clone } from "./node/clone"
+export { computedProp } from "./node/computedProp"
+export { type Context, createContext } from "./node/context"
+export {
+  assertIsNode,
+  isNode,
+  isFrozenNode,
+  node,
+  onDeepChange,
+  type NodeChange,
+  type NodeChangeListener,
+} from "./node/node"
+export { substituteNodeKeys } from "./node/substituteNodeKeys"
 export type { NodeKeyGenerator } from "./node/utils/nodeKeyGenerator"
-export { type VolatileProp, volatileProp } from "./node/volatileProp"
+export { volatileProp, type VolatileProp } from "./node/volatileProp"
 
 // plainTypes
 export type { Primitive } from "./plainTypes/types"
-export { connectReduxDevTools } from "./redux/connectReduxDevTools"
+
 // redux
 export { asReduxStore, type ReduxStore } from "./redux/redux"
+export { connectReduxDevTools } from "./redux/connectReduxDevTools"
 
 // transforms
 export { arrayToSetTransform } from "./transforms/arrayToSetTransform"
+export { objectToMapTransform } from "./transforms/objectToMapTransform"
+export { timestampToDateTransform } from "./transforms/timestampToDateTransform"
+export { dateToTimestampTransform } from "./transforms/dateToTimestampTransform"
+export { isoStringToDateTransform } from "./transforms/isoStringToDateTransform"
+export { dateToIsoStringTransform } from "./transforms/dateToIsoStringTransform"
+export { stringToBigIntTransform } from "./transforms/stringToBigIntTransform"
+export { bigIntToStringTransform } from "./transforms/bigIntToStringTransform"
 export { asMap } from "./transforms/asMap"
 export { asSet } from "./transforms/asSet"
-export { bigIntToStringTransform } from "./transforms/bigIntToStringTransform"
-export { dateToIsoStringTransform } from "./transforms/dateToIsoStringTransform"
-export { dateToTimestampTransform } from "./transforms/dateToTimestampTransform"
 export { ImmutableDate } from "./transforms/ImmutableDate"
-export { isoStringToDateTransform } from "./transforms/isoStringToDateTransform"
-export { objectToMapTransform } from "./transforms/objectToMapTransform"
-export { stringToBigIntTransform } from "./transforms/stringToBigIntTransform"
-export { timestampToDateTransform } from "./transforms/timestampToDateTransform"
 
 // utils
 export { deepEquals } from "./utils/deepEquals"
+
+// polyfills
+;(Symbol as any).dispose ??= Symbol("Symbol.dispose")
 
 // internal utils for mobx-bonsai packages
 export type { Dispose as _Dispose } from "./utils/disposable"
